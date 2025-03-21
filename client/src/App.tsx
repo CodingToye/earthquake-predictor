6a5b229c78c5212e1cf69b89f77d4219fbf431bc
Nick Toye <nick@pixelsandcode.co.uk>
2025-03-21 16:58:55 +0000

    Initial commit - project structure with client setup

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-sky-500">
+        <h1>Earthquake Predictor 123</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
