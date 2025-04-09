8ea873afcc3b7474657c3b0008ec1c8b50ed7436
Nick Toye <nick@pixelsandcode.co.uk>
2025-04-09 14:05:09 +0100

    testing endpoints2

--- server/app.py
+++ server/app.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
-# from pydantic import BaseModel
+from pydantic import BaseModel
 
 app = FastAPI()
 
-class EarthquakeFeatures():
+class EarthquakeFeatures(BaseModel):
     latitude: float
     longitude: float
     depth: float
@@ -11,7 +11,3 @@ class EarthquakeFeatures():
 @app.post("/predict")
 async def predict(features: EarthquakeFeatures):
     return {"received": features.dict()}
-
-@app.get("/ping")
-def ping():
-    return {"status": "pong"}
